2a7cc87dfaa6df92bb4602dd0c68d84c3e9dd05e
Daniel Knights <danknights95@gmail.com>
2021-02-04 00:07:14 +0000

    Type declarations

--- lib/drag-selectjs.d.ts
+++ lib/drag-selectjs.d.ts
@@ -1,6 +1,18 @@
 declare module 'drag-selectjs' {
-    function init(dragBoxClass?: string, selectedClass?: string): void
-    function onSelected(cb: () => unknown): void
+    /**
+     * Initialises drag-select.
+     * @param dragBoxClass - Class applied to the drag-box.
+     * @param selectedClass - Class applied to any selected elements.
+     */
+    export function init(dragBoxClass?: string, selectedClass?: string): void
 
-    export { init, onSelected }
+    /**
+     * Callback which is fired each time any elements are selected.
+     * @param cb - Callback to be run. Is passed a single parameter which is an array of selected elements.
+     * @example
+     * onSelected(selected => {
+     *   console.log(selected) // [div.__dragselect--selected]
+     * })
+     */
+    export function onSelected(cb: () => unknown): void
 }
